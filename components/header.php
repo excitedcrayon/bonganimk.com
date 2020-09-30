6a3866407198497ee72625f6cd33c39cee522af1
excitedcrayon <bonganimkonto@gmail.com>
2020-09-30 23:42:21 +1000

    Added favicon icon image

--- components/header.php
+++ components/header.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" type="image/x-icon" href="favicon.ico">
     <link rel="stylesheet" type="text/css" href="./css/style.css">
     <script type="text/javascript" src="./js/main.js"></script>
     <title>Bongani Mkonto &copy; 2020</title>
